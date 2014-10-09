27ab5b022dec68f18d07988b97d65ec8fd8db83e
Harris Lapiroff <harris@chromamine.com>
2014-10-09 16:13:46 -0400

    [hints] Check url safety before redirecting. Safety first!

--- zenaida/contrib/hints/views.py
+++ zenaida/contrib/hints/views.py
@@ -1,8 +1,11 @@
 from zenaida.contrib.hints.models import Dismissed
 from zenaida.contrib.hints.forms import DismissHintForm
 
+from django.core.exceptions import SuspiciousOperation
 from django.http import (HttpResponse, HttpResponseNotAllowed,
                         HttpResponseBadRequest, HttpResponseRedirect)
+from django.utils.http import is_safe_url
+
 
 def dismiss(request):
     if not request.POST:
@@ -16,4 +19,8 @@ def dismiss(request):
             next_url = request.GET['next']
         else:
             next_url = request.META['HTTP_REFERER']
+
+        if not is_safe_url(next_url, host=request.get_host()):
+            raise SuspiciousOperation("Url {} is not safe to redirect to.".format(next_url))
+
         return HttpResponseRedirect(next_url)
